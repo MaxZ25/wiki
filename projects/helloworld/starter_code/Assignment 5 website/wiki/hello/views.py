fcd1b69d461f38c6359be16ecd2c0126eab148a7
MaxZ25 <89923865+MaxZ25@users.noreply.github.com>
2022-05-11 20:33:48 -0400

    second update

--- projects/helloworld/starter_code/Assignment 5 website/wiki/hello/views.py
+++ projects/helloworld/starter_code/Assignment 5 website/wiki/hello/views.py
@@ -1,6 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from . import util
 
 # Create your views here.
 def index(request):
     return render(request, "hello/index.html")
+
+def getpage(request, name):
+    if (util.get_entry(name) == None):
+        return render(request, "hello/error.html")
+    
+    return render(request, "hello/entries.html", {
+        "view_name": util.get_entry(name)
+    })
